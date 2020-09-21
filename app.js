793e80efa22a2d84def4cd9e9e97547039acbf39
YongShengLee <leeyongsheng2107@gmail.com>
2020-09-21 14:33:33 +0800

    2nd deployment.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const https = require("https");
-const { timeStamp } = require('console');
+const swal = require('sweetalert');
 
 const app = express();
 
@@ -26,21 +26,6 @@ const ItemSchema = new mongoose.Schema ({
 
 const Item = mongoose.model("Item", ItemSchema);
 
-//default item to show how to do with to do list
-const item1 = new Item({
-  itemName: "Welcome to your todolist."
-  });
-
-  const item2 = new Item({
-  itemName: "Hit the add button to add a new to-do item."
-  });
-
-  const item3 = new Item({
-  itemName: "Hit the delete button to delete a to-do item."
-  });
-
-  const defaultItems = [item1, item2, item3];
-
 const signupSchema = new mongoose.Schema ({
   firstname: String,
   lastname: String,
@@ -55,6 +40,12 @@ const User = new mongoose.model("user", signupSchema);
 /********************** weather information ****************************/
 
 //weather API
+var weatherdata;
+var temp;
+var description;
+var icon;
+var imageurl;
+
 
 
 /************************ Home Route *******************/
@@ -66,18 +57,31 @@ app.get("/", function(req, res){
   https.get(url, function(response){
     console.log(response.statusCode);
     response.on("data", function(data){
-      const weatherdata = JSON.parse(data);
-      const temp = weatherdata.main.temp;
-      const description = weatherdata.weather[0].description;
+      weatherdata = JSON.parse(data);
+      currentTime = weatherdata.dt;
+
+      var date = new Date(currentTime * 1000);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+      temp = weatherdata.main.temp;
+      description = weatherdata.weather[0].description;
       const country = weatherdata.name;
-      const icon = weatherdata.weather[0].icon;
-      const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      console.log(temp);
+      icon = weatherdata.weather[0].icon;
+      imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.render("home",
       {description: description,
-        country: defaultCity,
-        temp: temp,
-        url: imageurl});
+       country: defaultCity,
+       temp: temp,
+       url: imageurl,
+       time: formattedTime});
     });
   });
 
@@ -96,20 +100,33 @@ app.get("/register", function(req, res){
   https.get(url, function(response){
     console.log(response.statusCode);
     response.on("data", function(data){
-      const weatherdata = JSON.parse(data);
-      const temp = weatherdata.main.temp;
-      const description = weatherdata.weather[0].description;
+      weatherdata = JSON.parse(data);
+      currentTime = weatherdata.dt;
+
+      var date = new Date(currentTime * 1000);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+      temp = weatherdata.main.temp;
+      description = weatherdata.weather[0].description;
       const country = weatherdata.name;
-      const icon = weatherdata.weather[0].icon;
-      const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      console.log(temp);
+      icon = weatherdata.weather[0].icon;
+      imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.render("register",
       {description: description,
-        country: defaultCity,
-        temp: temp,
-        url: imageurl});
+       country: defaultCity,
+       temp: temp,
+       url1: imageurl,
+       time: formattedTime});
     });
-  });
+})
 })
 
 app.post("/register", function(req, res){
@@ -119,22 +136,25 @@ app.post("/register", function(req, res){
     email: req.body.email,
     password: req.body.password
   });
-  user.save();
-    //   User.insertOne(user, function(err){
-    //   if (err) {
-    //   console.log(err);
-    //   }
-    //   else {
-    //   console.log("Successfully saved into Database.");
-    //   }
-    // });
-  res.redirect("/");
+  User.findOne({email: user.email}, function(err, foundExistingUser){
+    //email already registered
+    if(foundExistingUser){
+      console.log("Email has already been registered. Please register with another email.");
+      swal("Email has already been registered. Please register with another email.", "haha", "warning");
+      res.redirect("/register");
+    }
+    else{
+      user.save();
+      res.redirect("/");
+    }
+  })
+  
 
 });
 
 /*********************************************************/
 
-
+/************************ Login Route *******************/
 app.get("/login", function(req, res){
   const defaultCity = "Singapore";
   const units = "metric";
@@ -143,29 +163,42 @@ app.get("/login", function(req, res){
   https.get(url, function(response){
     console.log(response.statusCode);
     response.on("data", function(data){
-      const weatherdata = JSON.parse(data);
-      const temp = weatherdata.main.temp;
-      const description = weatherdata.weather[0].description;
+      weatherdata = JSON.parse(data);
+      currentTime = weatherdata.dt;
+
+      var date = new Date(currentTime * 1000);
+      // Hours part from the timestamp
+      var hours = date.getHours();
+      // Minutes part from the timestamp
+      var minutes = "0" + date.getMinutes();
+      // Seconds part from the timestamp
+      var seconds = "0" + date.getSeconds();
+
+      // Will display time in 10:30:23 format
+      var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+      temp = weatherdata.main.temp;
+      description = weatherdata.weather[0].description;
       const country = weatherdata.name;
-      const icon = weatherdata.weather[0].icon;
-      const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-      console.log(temp);
+      icon = weatherdata.weather[0].icon;
+      imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.render("login",
       {description: description,
-        country: defaultCity,
-        temp: temp,
-        url: imageurl});
+       country: defaultCity,
+       temp: temp,
+       url: imageurl,
+       time: formattedTime});
     });
-  });
 });
+});
+
+//global variable to check email
 var email;
 var password;
 
 app.post("/login", function(req,res){
     email =  req.body.login_email;
     password =  req.body.login_password;
-
-    console.log(email + " " + password);
 
     User.findOne({email: email}, function(err, foundUser){
       if(err) {
@@ -193,6 +226,7 @@ app.post("/login", function(req,res){
     });
 });
 
+/*********************************************************/
 
 app.get("/failed", function(req, res){
   const defaultCity = "Singapore";
@@ -202,12 +236,12 @@ app.get("/failed", function(req, res){
   https.get(url, function(response){
     console.log(response.statusCode);
     response.on("data", function(data){
-      const weatherdata = JSON.parse(data);
-      const temp = weatherdata.main.temp;
-      const description = weatherdata.weather[0].description;
+      weatherdata = JSON.parse(data);
+      temp = weatherdata.main.temp;
+      description = weatherdata.weather[0].description;
       const country = weatherdata.name;
-      const icon = weatherdata.weather[0].icon;
-      const imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      icon = weatherdata.weather[0].icon;
+      imageurl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
       console.log(temp);
       res.render("todolist",
       {description: description,
@@ -218,12 +252,14 @@ app.get("/failed", function(req, res){
   });
 });
 
+/************************ To Do List Route *******************/
 
-app.get("/todolist", function(req,res){
+app.get("/todolist", function(req, res){
   var defaultCity = "Singapore";
   var units = "metric";
-  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + defaultCity +"&appid=" + process.env.APIKEY + "&units=" + units;
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=" + defaultCity +"&appid=" + process.env.APIKEY + "&units=" + units + "exclude=current";
 
+ 
   console.log("Welcome, " + email);
 
     User.findOne({email: email}, function(err, foundUser){
@@ -276,11 +312,9 @@ app.post("/todolist", function(req,res){
     }
     
   })
-
-  const defaultCity = "Singapore";
-  const units = "metric";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + defaultCity +"&appid=" + process.env.APIKEY + "&units=" + units;
 });
+
+/******************************************************/
 
 app.post("/delete", function(req, res){
   const itemId = req.body.checkId;
@@ -295,7 +329,7 @@ app.post("/delete", function(req, res){
       console.log(err);
     }
   })
-})
+});
 
 
 
@@ -309,3 +343,5 @@ if(port == null || port == ""){
 app.listen(port, function() {
   console.log("Server started on port 3000.");
 });
+
+
